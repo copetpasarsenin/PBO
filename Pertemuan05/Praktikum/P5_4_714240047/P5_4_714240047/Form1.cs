638a9fa2ee821d7216f1334b57490ec51d148399
copetpasarsenin <richardfirmansyah17@gmail.com>
2025-11-10 17:46:21 +0700

    Add files via upload

---
+++ Pertemuan05/Praktikum/P5_4_714240047/P5_4_714240047/Form1.cs
@@ -0,0 +1,353 @@
+﻿namespace P5_4_714240047
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textNama = new System.Windows.Forms.TextBox();
+            this.comboJnsKelamin = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBoxPanahan = new System.Windows.Forms.CheckBox();
+            this.checkBoxYoga = new System.Windows.Forms.CheckBox();
+            this.checkBoxRenang = new System.Windows.Forms.CheckBox();
+            this.checkBoxTenis = new System.Windows.Forms.CheckBox();
+            this.checkBoxBulutangkis = new System.Windows.Forms.CheckBox();
+            this.checkBoxVoli = new System.Windows.Forms.CheckBox();
+            this.checkBoxBasket = new System.Windows.Forms.CheckBox();
+            this.checkBoxSepakBola = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.btnTampilkan = new System.Windows.Forms.Button();
+            this.btnSelesai = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(267, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(284, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FORM PENDAFTARAN";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(172, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(172, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Jenis Kelamin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(172, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tanggal Lahir";
+            // 
+            // textNama
+            // 
+            this.textNama.Location = new System.Drawing.Point(357, 107);
+            this.textNama.Name = "textNama";
+            this.textNama.Size = new System.Drawing.Size(224, 22);
+            this.textNama.TabIndex = 5;
+            // 
+            // comboJnsKelamin
+            // 
+            this.comboJnsKelamin.FormattingEnabled = true;
+            this.comboJnsKelamin.Items.AddRange(new object[] {
+            "Laki - Laki",
+            "Perempuan"});
+            this.comboJnsKelamin.Location = new System.Drawing.Point(357, 140);
+            this.comboJnsKelamin.Name = "comboJnsKelamin";
+            this.comboJnsKelamin.Size = new System.Drawing.Size(224, 24);
+            this.comboJnsKelamin.TabIndex = 6;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(357, 170);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(224, 22);
+            this.dateTimePicker.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBoxPanahan);
+            this.groupBox1.Controls.Add(this.checkBoxYoga);
+            this.groupBox1.Controls.Add(this.checkBoxRenang);
+            this.groupBox1.Controls.Add(this.checkBoxTenis);
+            this.groupBox1.Controls.Add(this.checkBoxBulutangkis);
+            this.groupBox1.Controls.Add(this.checkBoxVoli);
+            this.groupBox1.Controls.Add(this.checkBoxBasket);
+            this.groupBox1.Controls.Add(this.checkBoxSepakBola);
+            this.groupBox1.Location = new System.Drawing.Point(85, 233);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(338, 232);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pilihan Kelas";
+            // 
+            // checkBoxPanahan
+            // 
+            this.checkBoxPanahan.AutoSize = true;
+            this.checkBoxPanahan.Location = new System.Drawing.Point(178, 180);
+            this.checkBoxPanahan.Name = "checkBoxPanahan";
+            this.checkBoxPanahan.Size = new System.Drawing.Size(83, 20);
+            this.checkBoxPanahan.TabIndex = 0;
+            this.checkBoxPanahan.Text = "Panahan";
+            this.checkBoxPanahan.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxYoga
+            // 
+            this.checkBoxYoga.AutoSize = true;
+            this.checkBoxYoga.Location = new System.Drawing.Point(49, 180);
+            this.checkBoxYoga.Name = "checkBoxYoga";
+            this.checkBoxYoga.Size = new System.Drawing.Size(62, 20);
+            this.checkBoxYoga.TabIndex = 0;
+            this.checkBoxYoga.Text = "Yoga";
+            this.checkBoxYoga.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxRenang
+            // 
+            this.checkBoxRenang.AutoSize = true;
+            this.checkBoxRenang.Location = new System.Drawing.Point(49, 86);
+            this.checkBoxRenang.Name = "checkBoxRenang";
+            this.checkBoxRenang.Size = new System.Drawing.Size(77, 20);
+            this.checkBoxRenang.TabIndex = 0;
+            this.checkBoxRenang.Text = "Renang";
+            this.checkBoxRenang.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxTenis
+            // 
+            this.checkBoxTenis.AutoSize = true;
+            this.checkBoxTenis.Location = new System.Drawing.Point(49, 133);
+            this.checkBoxTenis.Name = "checkBoxTenis";
+            this.checkBoxTenis.Size = new System.Drawing.Size(63, 20);
+            this.checkBoxTenis.TabIndex = 0;
+            this.checkBoxTenis.Text = "Tenis";
+            this.checkBoxTenis.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxBulutangkis
+            // 
+            this.checkBoxBulutangkis.AutoSize = true;
+            this.checkBoxBulutangkis.Location = new System.Drawing.Point(178, 86);
+            this.checkBoxBulutangkis.Name = "checkBoxBulutangkis";
+            this.checkBoxBulutangkis.Size = new System.Drawing.Size(98, 20);
+            this.checkBoxBulutangkis.TabIndex = 0;
+            this.checkBoxBulutangkis.Text = "Bulutangkis";
+            this.checkBoxBulutangkis.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxVoli
+            // 
+            this.checkBoxVoli.AutoSize = true;
+            this.checkBoxVoli.Location = new System.Drawing.Point(178, 133);
+            this.checkBoxVoli.Name = "checkBoxVoli";
+            this.checkBoxVoli.Size = new System.Drawing.Size(52, 20);
+            this.checkBoxVoli.TabIndex = 0;
+            this.checkBoxVoli.Text = "Voli";
+            this.checkBoxVoli.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxBasket
+            // 
+            this.checkBoxBasket.AutoSize = true;
+            this.checkBoxBasket.Location = new System.Drawing.Point(178, 43);
+            this.checkBoxBasket.Name = "checkBoxBasket";
+            this.checkBoxBasket.Size = new System.Drawing.Size(71, 20);
+            this.checkBoxBasket.TabIndex = 0;
+            this.checkBoxBasket.Text = "Basket";
+            this.checkBoxBasket.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSepakBola
+            // 
+            this.checkBoxSepakBola.AutoSize = true;
+            this.checkBoxSepakBola.Location = new System.Drawing.Point(49, 43);
+            this.checkBoxSepakBola.Name = "checkBoxSepakBola";
+            this.checkBoxSepakBola.Size = new System.Drawing.Size(100, 20);
+            this.checkBoxSepakBola.TabIndex = 0;
+            this.checkBoxSepakBola.Text = "Sepak Bola";
+            this.checkBoxSepakBola.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(457, 233);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(335, 232);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Pilihan Jadwal";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(61, 179);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(150, 20);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Minggu, 13.00 - 20.00";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(61, 132);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(214, 20);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Jum\'at s/d Minggu, 09.00 - 11.00";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(61, 85);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(211, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Selasa s/d Kamis, 14.00 - 16.00";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(61, 42);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(198, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Senin s/d Rabu, 14.00 - 16.00";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // btnTampilkan
+            // 
+            this.btnTampilkan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTampilkan.Location = new System.Drawing.Point(304, 492);
+            this.btnTampilkan.Name = "btnTampilkan";
+            this.btnTampilkan.Size = new System.Drawing.Size(119, 31);
+            this.btnTampilkan.TabIndex = 10;
+            this.btnTampilkan.Text = "Tampilkan";
+            this.btnTampilkan.UseVisualStyleBackColor = true;
+            this.btnTampilkan.Click += new System.EventHandler(this.btnTampilkan_Click);
+            // 
+            // btnSelesai
+            // 
+            this.btnSelesai.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelesai.Location = new System.Drawing.Point(457, 492);
+            this.btnSelesai.Name = "btnSelesai";
+            this.btnSelesai.Size = new System.Drawing.Size(94, 31);
+            this.btnSelesai.TabIndex = 11;
+            this.btnSelesai.Text = "Selesai";
+            this.btnSelesai.UseVisualStyleBackColor = true;
+            this.btnSelesai.Click += new System.EventHandler(this.btnSelesai_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(870, 555);
+            this.Controls.Add(this.btnSelesai);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.comboJnsKelamin);
+            this.Controls.Add(this.textNama);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnTampilkan);
+            this.Name = "Form1";
+            this.Text = "ULBI SPORT SCHOOL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textNama;
+        private System.Windows.Forms.ComboBox comboJnsKelamin;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBoxPanahan;
+        private System.Windows.Forms.CheckBox checkBoxYoga;
+        private System.Windows.Forms.CheckBox checkBoxRenang;
+        private System.Windows.Forms.CheckBox checkBoxTenis;
+        private System.Windows.Forms.CheckBox checkBoxBulutangkis;
+        private System.Windows.Forms.CheckBox checkBoxVoli;
+        private System.Windows.Forms.CheckBox checkBoxBasket;
+        private System.Windows.Forms.CheckBox checkBoxSepakBola;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button btnTampilkan;
+        private System.Windows.Forms.Button btnSelesai;
+    }
+}
