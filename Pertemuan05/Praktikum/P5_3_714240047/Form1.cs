042360dbf0bea832355ed30320a4e238682802f4
copetpasarsenin <richardfirmansyah17@gmail.com>
2025-11-10 22:59:44 +0700

    Add files via upload

---
+++ Pertemuan05/Praktikum/P5_3_714240047/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P5_3_714240047
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string os = "";
+            string status = "";
+
+            if (rb_android.Checked == true)
+            {
+                os = "Android";
+            } 
+            else if (rb_ios.Checked == true)
+            {
+                os = "iOS";
+            }
+
+            if (cbYa.Checked == true)
+            {
+                status = "Ya, sudah diperbaiki"; 
+            }
+            else if (cbYa.Checked == false)
+            {
+                status = "Belum diperbaiki";
+            }
+            MessageBox.Show(
+                "Merek HP: " + txtMerkHP.Text + 
+                "\nSistem Operasi: " + os +
+                "\nStastus Perbaikan: " + status,
+                "Informasi Service HP",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnKeluar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
