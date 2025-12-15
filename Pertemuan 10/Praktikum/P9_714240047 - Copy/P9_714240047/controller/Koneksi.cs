0809af78071ed64e22454f89686cc5bd9237350b
copetpasarsenin <richardfirmansyah57@gmail.com>
2025-12-15 15:25:16 +0700

    aa

---
+++ Pertemuan 10/Praktikum/P9_714240047 - Copy/P9_714240047/controller/Koneksi.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace P9_714240047.controller
+{
+    internal class Koneksi
+    {
+        string connectionstring = "Server=localhost; Database=pemrog2ulbi; Uid=root; Pwd=;";
+        public MySqlConnection kon;
+
+        public void OpenConnection()
+        {
+            try
+            {
+                kon = new MySqlConnection(connectionstring);
+                kon.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Koneksi: " + ex.Message);
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (kon != null && kon.State == ConnectionState.Open)
+            {
+                kon.Close();
+            }
+        }
+
+        public DataTable ShowData(string query)
+        {
+            DataTable table = new DataTable();
+            try
+            {
+                OpenConnection();
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, kon);
+                adapter.Fill(table);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error ShowData: " + ex.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+            return table;
+        }
+
+        public void ExecuteQuery(MySqlCommand command)
+        {
+            command.Connection = kon;
+            command.ExecuteNonQuery();
+        }
+    }
+}
